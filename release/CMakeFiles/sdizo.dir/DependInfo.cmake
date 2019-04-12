f88e70e4aa5314e1479eb7e7967feb2b2e0ae4f5
secmeant <secmeant@gmail.com>
2019-04-12 07:15:59 -0700

    adding compiled

---
+++ release/CMakeFiles/sdizo.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/patry/Desktop/pwr-sdizo/src/array.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/array.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/heap.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/heap.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/list.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/list.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/menu.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/menu.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/redblacktree.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/redblacktree.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/tests.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/tests.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/tree.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/tree.cc.obj"
+  "C:/Users/patry/Desktop/pwr-sdizo/src/treeprinter.cc" "C:/Users/patry/Desktop/pwr-sdizo/release/CMakeFiles/sdizo.dir/src/treeprinter.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FMT_HEADER_ONLY=1"
+  "FMT_SHARED"
+  "SYSTEM_WINDOWS"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_SCL_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/patry/Desktop/vcpkg/installed/x86-windows/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ release/CMakeFiles/sdizo.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdizo.dir/src/array.cc.obj"
+  "CMakeFiles/sdizo.dir/src/heap.cc.obj"
+  "CMakeFiles/sdizo.dir/src/list.cc.obj"
+  "CMakeFiles/sdizo.dir/src/menu.cc.obj"
+  "CMakeFiles/sdizo.dir/src/redblacktree.cc.obj"
+  "CMakeFiles/sdizo.dir/src/tests.cc.obj"
+  "CMakeFiles/sdizo.dir/src/tree.cc.obj"
+  "CMakeFiles/sdizo.dir/src/treeprinter.cc.obj"
+  "sdizo.pdb"
+  "sdizo.exe"
+  "sdizo.exe.manifest"
+  "sdizo.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdizo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
