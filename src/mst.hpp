5785e718340e8764f8d39730f7032289b40ee5b9
secmeant <secmeant@gmail.com>
2019-05-28 14:55:51 +0200

    Edge is now using two nodes

--- src/mst.hpp
+++ src/mst.hpp
@@ -2,27 +2,30 @@
 #include <cstdint>
 
 namespace sdizo2{
-  struct Edge
+struct Edge
+{
+  int32_t v1;
+  int32_t v2;
+  int32_t weight;
+
+  inline Edge() : v1{0}, v2{0}, weight{0} {}
+  inline Edge(int32_t v1, int32_t v2, int32_t weight)
+  : v1{v1}, v2{v2}, weight{weight} {}
+
+  inline int32_t getKey()
+  {return weight;}
+
+  inline bool operator==(Edge e)
   {
-    int32_t vertex;
-    int32_t weight;
-
-    inline Edge() : vertex{0}, weight{0} {}
-    inline Edge(int32_t vertex, int32_t weight)
-    : vertex{vertex}, weight{weight} {}
-
-    inline int32_t getKey()
-    {return weight;}
-
-    inline bool operator==(const Edge& e)
-    {
-      return this->vertex == e.vertex &&
+    return ((this->v1 == e.v1 && this->v2 == e.v2)  ||
+            (this->v1 == e.v2 && this->v2 == e.v1)) &&
              this->weight == e.weight;
-    }
+  }
 
-    inline bool operator!=(const Edge& e)
-    {
-      return !(*this == e);
-    }
-  };
+  inline bool operator!=(Edge e)
+  {
+    return !(*this == e);
+  }
 };
+}; // namespace sdizo2
+
